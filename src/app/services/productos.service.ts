949a7e80f7ec5bf833f14e3288f99aca5651cf28
Paul Broca <paul.broca.rmz@gmail.com>
2019-09-12 12:47:01 -0500

    listo para producción

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
-import { setTimeout } from 'timers';
+import { resolve } from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductosService {
 
   cargando = true;
   pruductos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
 
@@ -19,15 +21,55 @@ export class ProductosService {
 
   private cargarProductos() {
 
-    this.http.get('https://angular-html-36f27.firebaseio.com/productos_idx.json')
-      .subscribe( ( resp: Producto[]) => {
-        console.log(resp);
-        this.pruductos = resp;
+    return new Promise( ( resolve, reject)=> {
 
-        setTimeout(() => {
+      this.http.get('https://angular-html-36f27.firebaseio.com/productos_idx.json')
+      .subscribe( ( resp: Producto[]) => {
+        this.pruductos = resp;
         this.cargando = false;
-        }, 200);
+        resolve();
       });
 
+    });
+
+   
   }
+    getProducto( id: string ) {
+
+      return this.http.get(`https://angular-html-36f27.firebaseio.com/productos/${ id }.json`);
+
+    }
+
+    buscarProducto( termino: string ){
+
+      if ( this.pruductos.length ===0 ) {
+        // cargar productos
+        this.cargarProductos().then(  ()=> {
+          // ejecutar despues de tener los productos
+          // aplicar filtro
+          this.filtrarProductos( termino );
+        });
+
+      } else {
+          // aplicar el fintro
+          this.filtrarProductos( termino );
+      }
+
+    }
+    private filtrarProductos( termino: string ) {
+
+      // console.log(this.pruductos);
+      this.productosFiltrado = [];
+
+      termino = termino.toLocaleLowerCase();
+
+      this.pruductos.forEach( prod => {
+
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+
+        if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+          this.productosFiltrado.push( prod );
+        }
+      } );
+    }
 }
